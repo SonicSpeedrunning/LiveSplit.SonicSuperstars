6e64e938442458bda53292dea6c43fe682aef4c6
Jujstme <just.tribe@gmail.com>
2023-10-17 23:31:25 +0200

    Implemented autosplit and load removal

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,11 @@
 
 use asr::{
     future::{next_tick, retry},
-    game_engine::unity::{il2cpp::{Module, Version, UnityPointer, Image}, SceneManager, get_scene_name},
+    game_engine::unity::{
+        get_scene_name,
+        il2cpp::{Image, Module, UnityPointer, Version},
+        SceneManager,
+    },
     time::Duration,
     timer::{self, TimerState},
     watcher::Watcher,
@@ -93,18 +97,26 @@ struct Settings {
     #[default = true]
     /// => Enable auto start
     start: bool,
-            // settings to be added for each level
+    #[default = true]
+    /// Split after completion of each level
+    split: bool,
 }
 
 #[derive(Default)]
 struct Watchers {
+    start_trigger: Watcher<bool>,
+    level_id: Watcher<u32>,
     is_loading: Watcher<bool>,
+    goal_ring_flag: Watcher<bool>,
 }
 
 struct Memory {
     il2cpp_module: Module,
     game_assembly: Image,
     scene_manager: SceneManager,
+    is_loading: UnityPointer<1>,
+    current_scene_controller: UnityPointer<2>,
+    next_scene_name: UnityPointer<3>,
 }
 
 impl Memory {
@@ -112,43 +124,101 @@ impl Memory {
         let il2cpp_module = Module::attach(game, Version::V2020)?;
         let game_assembly = il2cpp_module.get_default_image(game)?;
         let scene_manager = SceneManager::attach(game)?;
-        
-        //let is_loading = UnityPointer::new("SceneManager", 1, &["s_sInstance", "m_bProcessing"]);
+
+        let is_loading =
+            UnityPointer::new("Scene_Manager", 0, &["<IsTransitionPlay>k__BackingField"]);
+
+        let current_scene_controller = UnityPointer::new(
+            "Scene_Manager",
+            2,
+            &["s_Instance", "<CurrentSceneController>k__BackingField"],
+        );
+
+        let next_scene_name = UnityPointer::new(
+            "Scene_Manager",
+            2,
+            &["s_Instance", "<NextSceneName>k__BackingField"],
+        );
 
         Some(Self {
             il2cpp_module,
             game_assembly,
             scene_manager,
+            is_loading,
+            current_scene_controller,
+            next_scene_name,
         })
     }
 }
 
 fn update_loop(game: &Process, addresses: &Memory, watchers: &mut Watchers) {
-    watchers.is_loading.update_infallible({
-        let scene_path = addresses.scene_manager.get_current_scene_path::<128>(game);
-
-        let scene_name = match &scene_path {
-            Ok(x) => Some(get_scene_name(x)),
-            _ => None,
-        };
-
-        match scene_name {
-            Some(b"ReleaseScene") | Some(b"GameMain") => true,
-            Some(_) => false,
-            None => match &watchers.is_loading.pair {
-                Some(x) => x.current,
-                _ => false,
+    watchers.start_trigger.update_infallible(
+        if let Ok(scene_path) = addresses.scene_manager.get_current_scene_path::<128>(game) {
+            let scene_name = get_scene_name(&scene_path);
+            if scene_name == b"MainMenu" {
+                let next_scene = game
+                    .read_pointer_path64::<[u16; 10]>(
+                        addresses
+                            .next_scene_name
+                            .deref_offsets(game, &addresses.il2cpp_module, &addresses.game_assembly)
+                            .unwrap_or_default(),
+                        &[0, 0x14],
+                    )
+                    .unwrap_or_default()
+                    .map(|val| val as u8);
+                &next_scene == b"MovieScene"
+            } else {
+                false
             }
-        }
-    });
+        } else {
+            false
+        },
+    );
+
+    watchers.level_id.update_infallible(
+        game.read_pointer_path64(
+            addresses
+                .current_scene_controller
+                .deref_offsets(game, &addresses.il2cpp_module, &addresses.game_assembly)
+                .unwrap_or_default(),
+            &[0, 0x40, 0x10],
+        )
+        .unwrap_or_default(),
+    );
+
+    watchers.is_loading.update_infallible(
+        addresses
+            .is_loading
+            .deref::<bool>(game, &addresses.il2cpp_module, &addresses.game_assembly)
+            .unwrap_or_default(),
+    );
+
+    watchers.goal_ring_flag.update_infallible(
+        game.read_pointer_path64(
+            addresses
+                .current_scene_controller
+                .deref_offsets(game, &addresses.il2cpp_module, &addresses.game_assembly)
+                .unwrap_or_default(),
+            &[0, 0xF3],
+        )
+        .unwrap_or_default(),
+    );
 }
 
 fn start(watchers: &Watchers, settings: &Settings) -> bool {
-    false
+    settings.start
+        && watchers
+            .start_trigger
+            .pair
+            .is_some_and(|val| val.changed_to(&true))
 }
 
 fn split(watchers: &Watchers, settings: &Settings) -> bool {
-    false
+    settings.split
+        && watchers
+            .goal_ring_flag
+            .pair
+            .is_some_and(|val| val.changed_to(&false))
 }
 
 fn reset(_watchers: &Watchers, _settings: &Settings) -> bool {
