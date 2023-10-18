c2ac52dd4ac7ee07349eeb076f5af2dd2a278e5c
Jujstme <just.tribe@gmail.com>
2023-10-18 11:04:17 +0200

    Disabled autosplitting in time attack mode and made things more consistent in order to prevent unwanted splits

--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,12 @@
 use asr::{
     deep_pointer::DeepPointer,
     future::{next_tick, retry},
-    game_engine::unity::{
-        get_scene_name,
-        il2cpp::{Module, Version},
-        SceneManager,
-    },
+    game_engine::unity::il2cpp::{Module, Version},
+    string::ArrayCString,
     time::Duration,
     timer::{self, TimerState},
     watcher::Watcher,
-    Process,
+    Address, Address64, Process,
 };
 
 asr::panic_handler!();
@@ -112,71 +109,68 @@ struct Watchers {
 }
 
 struct Memory {
-    scene_manager: SceneManager,
     is_loading: DeepPointer<1>,
-    level_id: DeepPointer<4>,
     next_scene_name: DeepPointer<3>,
-    is_result_sequence: DeepPointer<3>,
+    current_scene_controller: DeepPointer<2>,
+    game_scene_controller_offsets: GameSceneControllerOffsets,
+}
+
+struct GameSceneControllerOffsets {
+    stage_info: u32,
+    is_goal_sequence: u32,
+    is_result_sequence: u32,
+    is_time_attack_mode: u32,
 }
 
 impl Memory {
     async fn init(game: &Process) -> Self {
         let il2cpp_module = Module::wait_attach(game, Version::V2020).await;
         let game_assembly = il2cpp_module.wait_get_default_image(game).await;
-        let scene_manager = SceneManager::wait_attach(game).await;
 
-        let scene_manager_class = game_assembly.wait_get_class(game, &il2cpp_module, "Scene_Manager").await;
-        let scene_manager_static = scene_manager_class.wait_get_static_table(game, &il2cpp_module).await;
-        let scene_manager_is_in_transition = scene_manager_class.wait_get_field_offset(
-            game,
-            &il2cpp_module,
-            "<IsTransitionPlay>k__BackingField",
-        ).await as _;
-
-        let scene_manager_parent = scene_manager_class
-            .wait_get_parent(game, &il2cpp_module).await
-            .wait_get_parent(game, &il2cpp_module).await;
-        let scene_manager_parent_static =
-            scene_manager_parent.wait_get_static_table(game, &il2cpp_module).await;
-        let scene_manager_parent_instance =
-            scene_manager_parent.wait_get_field_offset(game, &il2cpp_module, "s_Instance").await as _;
-        let scene_manager_current_scene_controller = scene_manager_class.wait_get_field_offset(
-            game,
-            &il2cpp_module,
-            "<CurrentSceneController>k__BackingField",
-        ).await as _;
-        let scene_manager_next_scene_name = scene_manager_class.wait_get_field_offset(
-            game,
-            &il2cpp_module,
-            "<NextSceneName>k__BackingField",
-        ).await as _;
-
-        let game_scene_controller =
-            game_assembly.wait_get_class(game, &il2cpp_module, "GameSceneControllerBase").await;
-        let game_scene_controller_stage_info =
-            game_scene_controller.wait_get_field_offset(game, &il2cpp_module, "stageInfo").await as _;
-        let game_scene_controller_is_result_sequence =
-            game_scene_controller.wait_get_field_offset(game, &il2cpp_module, "isResultSequence").await as _;
+        let scene_manager_class = game_assembly
+            .wait_get_class(game, &il2cpp_module, "Scene_Manager")
+            .await;
+        let scene_manager_static = scene_manager_class
+            .wait_get_static_table(game, &il2cpp_module)
+            .await;
+        let scene_manager_is_in_transition = scene_manager_class
+            .wait_get_field_offset(game, &il2cpp_module, "<IsTransitionPlay>k__BackingField")
+            .await as _;
 
         let is_loading =
             DeepPointer::new_64bit(scene_manager_static, &[scene_manager_is_in_transition]);
-        let level_id = DeepPointer::new_64bit(
+
+        let scene_manager_parent = scene_manager_class
+            .wait_get_parent(game, &il2cpp_module)
+            .await
+            .wait_get_parent(game, &il2cpp_module)
+            .await;
+        let scene_manager_parent_static = scene_manager_parent
+            .wait_get_static_table(game, &il2cpp_module)
+            .await;
+        let scene_manager_parent_instance = scene_manager_parent
+            .wait_get_field_offset(game, &il2cpp_module, "s_Instance")
+            .await as _;
+        let scene_manager_current_scene_controller = scene_manager_class
+            .wait_get_field_offset(
+                game,
+                &il2cpp_module,
+                "<CurrentSceneController>k__BackingField",
+            )
+            .await as _;
+
+        let current_scene_controller = DeepPointer::new_64bit(
             scene_manager_parent_static,
             &[
                 scene_manager_parent_instance,
                 scene_manager_current_scene_controller,
-                game_scene_controller_stage_info,
-                0x0,
             ],
         );
-        let is_result_sequence = DeepPointer::new_64bit(
-            scene_manager_parent_static,
-            &[
-                scene_manager_parent_instance,
-                scene_manager_current_scene_controller,
-                game_scene_controller_is_result_sequence,
-            ],
-        );
+
+        let scene_manager_next_scene_name = scene_manager_class
+            .wait_get_field_offset(game, &il2cpp_module, "<NextSceneName>k__BackingField")
+            .await as _;
+
         let next_scene_name = DeepPointer::new_64bit(
             scene_manager_parent_static,
             &[
@@ -186,46 +180,110 @@ impl Memory {
             ],
         );
 
+        let game_scene_controller = game_assembly
+            .wait_get_class(game, &il2cpp_module, "GameSceneControllerBase")
+            .await;
+        let game_scene_controller_stage_info = game_scene_controller
+            .wait_get_field_offset(game, &il2cpp_module, "stageInfo")
+            .await as _;
+        let game_scene_controller_is_goal_sequence = game_scene_controller
+            .wait_get_field_offset(game, &il2cpp_module, "isGoalSequence")
+            .await as _;
+        let game_scene_controller_is_result_sequence = game_scene_controller
+            .wait_get_field_offset(game, &il2cpp_module, "isResultSequence")
+            .await as _;
+
+        let game_scene_controller = game_assembly
+            .wait_get_class(game, &il2cpp_module, "GameSceneController")
+            .await;
+        let game_scene_controller_is_time_attack_mode = game_scene_controller
+            .wait_get_field_offset(game, &il2cpp_module, "isTimeAttackMode")
+            .await as _;
+
+        let game_scene_controller_offsets = GameSceneControllerOffsets {
+            stage_info: game_scene_controller_stage_info,
+            is_goal_sequence: game_scene_controller_is_goal_sequence,
+            is_result_sequence: game_scene_controller_is_result_sequence,
+            is_time_attack_mode: game_scene_controller_is_time_attack_mode,
+        };
+
         Self {
-            scene_manager,
             is_loading,
-            level_id,
             next_scene_name,
-            is_result_sequence,
+            current_scene_controller,
+            game_scene_controller_offsets,
         }
+    }
+
+    fn get_current_scene_controller_name<const N: usize>(
+        &self,
+        game: &Process,
+        scene_controller_address: Address,
+    ) -> Option<ArrayCString<N>> {
+        game.read_pointer_path64(scene_controller_address, &[0, 0x10, 0])
+            .ok()
     }
 }
 
 fn update_loop(game: &Process, addresses: &Memory, watchers: &mut Watchers) {
+    let current_scene_controller: Address = addresses
+        .current_scene_controller
+        .deref::<Address64>(game)
+        .unwrap_or_default()
+        .into();
+    let current_scene_controller_name = addresses
+        .get_current_scene_controller_name::<128>(game, current_scene_controller)
+        .unwrap_or_default();
+
     watchers.start_trigger.update_infallible(
-        if let Ok(scene_path) = addresses.scene_manager.get_current_scene_path::<128>(game) {
-            let scene_name = get_scene_name(&scene_path);
-            if scene_name == b"MainMenu" {
-                let next_scene = addresses
-                    .next_scene_name
-                    .deref::<[u16; 10]>(game)
-                    .unwrap_or_default()
-                    .map(|val| val as u8);
-                &next_scene == b"MovieScene"
-            } else {
-                false
-            }
-        } else {
-            false
-        },
+        current_scene_controller_name.matches(b"SelectSaveSlotController")
+            && addresses
+                .next_scene_name
+                .deref::<[u16; 10]>(game)
+                .is_ok_and(|name| &name.map(|val| val as u8) == b"MovieScene"),
     );
 
-    watchers
-        .level_id
-        .update_infallible(addresses.level_id.deref(game).unwrap_or_default());
+    watchers.level_id.update_infallible(
+        if current_scene_controller_name.matches(b"GameSceneController") {
+            game.read_pointer_path64(
+                current_scene_controller,
+                &[addresses.game_scene_controller_offsets.stage_info as _, 0],
+            )
+            .unwrap_or_default()
+        } else {
+            match &watchers.level_id.pair {
+                Some(x) => x.current,
+                _ => 0,
+            }
+        },
+    );
 
     watchers
         .is_loading
         .update_infallible(addresses.is_loading.deref(game).unwrap_or_default());
 
-    watchers
-        .goal_ring_flag
-        .update_infallible(addresses.is_result_sequence.deref(game).unwrap_or_default());
+    watchers.goal_ring_flag.update_infallible(
+        if current_scene_controller_name.matches(b"GameSceneController") {
+            let is_time_attack = game.read::<bool>(
+                current_scene_controller
+                    + addresses.game_scene_controller_offsets.is_time_attack_mode,
+            );
+            if is_time_attack.is_ok_and(|val| val) {
+                false
+            } else {
+                game.read(
+                    current_scene_controller
+                        + addresses.game_scene_controller_offsets.is_result_sequence,
+                )
+                .is_ok_and(|val| val)
+            }
+        } else {
+            match &watchers.goal_ring_flag.pair {
+                Some(x) => x.current,
+                _ => false,
+            }
+        },
+    );
 }
 
 fn start(watchers: &Watchers, settings: &Settings) -> bool {
